280d1746ed14405c13b84cdf2e602d50e86ede98
Houfeng <admin@xhou.net>
2015-09-24 21:10:48 +0800

    dev

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
-    return browserify('./src/javascript/app.js')
-        .bundle()
-        //Pass desired output filename to vinyl-source-stream
-        .pipe(source('bundle.js'))
-        // Start piping stream to tasks!
-        .pipe(gulp.dest('./build/'));
+	return browserify('./src/javascript/app.js')
+		.bundle()
+		//Pass desired output filename to vinyl-source-stream
+		.pipe(source('bundle.js'))
+		// Start piping stream to tasks!
+		.pipe(gulp.dest('./build/'));
 });
