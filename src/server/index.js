1e07149960ba751c9c6bb199252fd691ea08416c
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2017-03-10 23:17:51 +0800

    1.0.11

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,2 @@
 exports.version = '{version}';
 exports.Parser = require('../common/parser');
-exports.Client = require('../client');
-exports.Editor = require('../editor');
-exports.Toolbar = require('../toolbar');
-exports.Viewer = require('../viewer');
